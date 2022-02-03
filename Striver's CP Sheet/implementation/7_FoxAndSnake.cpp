71819591443f6f0f6140038c199b806be24fd0bf
nitin rathod <nitinrathod2300@gmail.com>
2022-02-03 21:26:21 +0530

    day one need to pracites again

---
+++ Striver's CP Sheet/implementation/7_FoxAndSnake.cpp
@@ -0,0 +1,24 @@
+// https://codeforces.com/problemset/problem/510/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, m;
+    cin>>n>>m;
+    bool aa = false;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(i%2==0){
+                cout<<"#";
+            }
+            else{
+                
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
