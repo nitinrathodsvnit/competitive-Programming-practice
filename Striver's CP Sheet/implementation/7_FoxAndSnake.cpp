80b5a9836740ea91135b83ccf1b660e6b7fc823c
nitin rathod <nitinrathod2300@gmail.com>
2022-02-04 08:36:05 +0530

    day 2, 7 done

--- Striver's CP Sheet/implementation/7_FoxAndSnake.cpp
+++ Striver's CP Sheet/implementation/7_FoxAndSnake.cpp
@@ -8,13 +8,22 @@ int main(){
     cin.tie(NULL);
     int n, m;
     cin>>n>>m;
-    bool aa = false;
+    // bool aa = false;
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
             if(i%2==0){
                 cout<<"#";
             }
             else{
+                if( (i+1)%4 ==0 and j==0){
+                    cout<<"#";
+                }
+                else if((i+1)%4 ==2 and j==m-1){
+                    cout<<"#";
+                }
+                else{
+                    cout<<".";
+                }
                 
             }
         }
