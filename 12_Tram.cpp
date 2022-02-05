2b140c041ecf95bc241cae6670046fcf3220633a
nitin rathod <nitinrathod2300@gmail.com>
2022-02-05 12:36:51 +0530

    day3, 12 done

---
+++ 12_Tram.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t, sum=0, mx=0;
+    cin>>t;
+    while (t--){
+        int a, b;
+        cin>>a>>b;
+        sum = sum-a+b;
+        mx= max(sum, mx);
+    }
+    cout<<sum<<endl;
+    return 0;
+}
---
+++ 12_nearly_lucky_number.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int c=0;
+    int n;
+    cin>>n;
+    string s = to_string(n);
+    for(int i=0; i<s.length(); i++){
+        if(s[i]=='4' or s[i]=='7'){
+            c++;
+        }
+    }
+    if(c%7==0 or c%4==0){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
