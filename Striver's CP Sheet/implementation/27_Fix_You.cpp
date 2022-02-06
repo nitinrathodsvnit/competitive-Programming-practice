23e680e5961633cafdd4a47534d0ca75d4b3023e
nitin rathod <nitinrathod2300@gmail.com>
2022-02-06 14:02:59 +0530

    day4, 27done

---
+++ Striver's CP Sheet/implementation/27_Fix_You.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+#define int            long long int
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define all(p)         p.begin(), p.end()
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define print(a)       for(auto x : a) cout << x << ' '; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << ' '  << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< ' '; cout << endl
+
+using namespace std;
+
+const int N = 200005;
+
+void solve(){
+    int n, m, ans=0;
+    cin>>n>>m;
+    char a[n][m];
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            cin>>a[i][j];
+            if(a[i][m-1]=='R'){
+                ans++;
+            }
+            if(a[n-1][j]=='D'){
+                ans++;
+            }
+        }  
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+    while (t--) solve();
+    return 0;
+}
