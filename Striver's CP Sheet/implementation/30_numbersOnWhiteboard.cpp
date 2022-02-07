9fd10165903537bc372d7ae46b855b718adccfc2
nitin rathod <nitinrathod2300@gmail.com>
2022-02-07 21:47:39 +0530

    day5, 30done

---
+++ Striver's CP Sheet/implementation/30_numbersOnWhiteboard.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<2<<endl;
+        cout<<n<<" "<<n-1<<endl;
+        for(int i=n; i>2; i--){
+            cout<<i<<" "<<i-2<<endl;
+        }
+    }
+    return 0;
+}
