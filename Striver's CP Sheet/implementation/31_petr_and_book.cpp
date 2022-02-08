e8f3132f2d9d47d7c57c7bc93c19f46cc028d56a
nitin rathod <nitinrathod2300@gmail.com>
2022-02-08 10:02:03 +0530

    day 6, 31done

---
+++ Striver's CP Sheet/implementation/31_petr_and_book.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, sum=0;
+    cin>>n;
+    int a[7];
+    for(int i=0;i<7; i++){
+        cin>>a[i];
+    }
+    int i=0;
+    while(n){
+        i %= 7;
+        n -= a[i];
+        n = max(n, 0);
+        i++;
+    }
+
+    cout<<i;
+    return 0;
+}
