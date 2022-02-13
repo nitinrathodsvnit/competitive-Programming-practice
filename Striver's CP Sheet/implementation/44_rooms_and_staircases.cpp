3f68d8de8d6b9af7d4ed1757ad2258f19d29da84
nitin rathod <nitinrathod2300@gmail.com>
2022-02-13 15:39:00 +0530

    day 10

---
+++ Striver's CP Sheet/implementation/44_rooms_and_staircases.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+#define int            long long int
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define all(p)         p.begin(), p.end()
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define print(a)       for(auto x : a) cout << x << ' '; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << ' '  << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< ' '; cout << endl
+
+using namespace std;
+
+const int N = 200005;
+
+void solve(){
+    int n, tm=0, m=0;
+    cin>>n;
+    string s;
+    cin>>s;
+    for(int i=0; i<n; i++){
+        if(s[i] == '1'){
+            tm = max(i, n-i-1);
+            m = max(m, tm);
+        }
+    }
+    cout<<(2*(tm+1))<<"\n";
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+    while (t--) solve();
+    return 0;
+}
