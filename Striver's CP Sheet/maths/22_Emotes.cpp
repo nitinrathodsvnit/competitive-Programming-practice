85f5add276a6a71c210db6754eafeb14f1a88c9b
nitin rathod <nitinrathod2300@gmail.com>
2022-02-22 10:23:31 +0530

    day 19

---
+++ Striver's CP Sheet/maths/22_Emotes.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n, m, k;
+	cin >> n >> m >> k;
+	
+	vector<int> a(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
+	}
+	sort(a.rbegin(), a.rend());
+	
+	cout << m / (k + 1) * 1ll * (a[0] * 1ll * k + a[1]) + m % (k + 1) * 1ll * a[0] << endl;
+	
+	return 0;
+}
