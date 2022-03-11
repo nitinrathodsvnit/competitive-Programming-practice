dec2f3865b0cf14da8e8ae02ff20b0f70c402547
nitin rathod <nitinrathod2300@gmail.com>
2022-03-11 13:06:03 +0530

    11/03 2

---
+++ C2-ladders/b_power_walking.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+#define int            long long int
+#define REP(i, a, b)   for(int i=a; i<=b; i++)
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define all(p)         p.begin(), p.end()
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define print(a)       for(auto x : a) cout << x << ' '; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << ' '  << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< ' '; cout << endl
+
+using namespace std;
+
+const int N = 200005;
+
+void solve(){
+    int n;
+    cin>>n;
+    set<int> s;
+    for(int i=0; i<n; i++){
+        int temp;
+        cin>>temp;
+        s.insert(temp);
+    }
+    int sz = s.size();
+    for(int i=1; i<=n; i++){
+        cout<<max(sz, i)<<" ";
+    }
+    cout<<"\n";
+    return;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+    while (t--) solve();
+    return 0;
+}
