29f5aeabc71095a93d8a6916513f488d8fdc9710
nitin rathod <nitinrathod2300@gmail.com>
2022-03-15 05:16:37 +0530

    15/03

---
+++ C2-ladders/b_mex_and_array.cpp
@@ -0,0 +1,24 @@
+// revisite
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (auto& u : a)
+            cin >> u;
+ 
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += (i + 1) * (n - i);
+            if (a[i] == 0)
+                ans += (i + 1) * (n - i);
+        }
+        cout << ans << '\n';
+    }
+}
