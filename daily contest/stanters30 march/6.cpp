1296adf868ebb3871b763b9f0e365feecfe4ddb3
nitin rathod <nitinrathod2300@gmail.com>
2022-03-16 22:49:00 +0530

    incomplate 6

---
+++ daily contest/stanters30 march/6.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+#define int            long long int
+#define REP(i, a, b)   for(int i=a; i<=b; i++)
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define all(p)         p.begin(), p.end()
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define print(a)       for(auto x : a) cout << x << ' '; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << ' '  << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< ' '; cout << endl
+
+using namespace std;
+
+const int N = 200005;
+
+void solve(){
+    int n, m;
+    cin>>n>>m;
+    int a[n][m];
+    int x1,y1, x2,y2;
+    cin>>x1>>y1>>x2>>y2;
+    if(abs(x1-x2)%2==1 and abs(y2-y1)%2==0){
+        if(y1%2==0){
+            
+        }
+    }
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+    while (t--) solve();
+    return 0;
+}
