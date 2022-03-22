8d7fe02826ac2de52460b4156656e6c2b66ba274
nitin rathod <nitinrathod2300@gmail.com>
2022-03-22 09:01:18 +0530

    22/03

---
+++ daily contest/Kick Start/2022 A round/1.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+
+#define int            long long int
+#define REP(i, a, b)   for(int i=a; i<=b; i++)
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define all(p)         p.begin(), p.end()
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define print(a)       for(auto x : a) cout << x << ' '; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << ' '  << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< ' '; cout << endl
+
+using namespace std;
+
+const int N = 200005;
+
+bool Substr(string s2, string s1)
+{
+    int counter = 0; // pointing s2
+    int i = 0;
+    for(;i<s1.length();i++)
+    {
+        if(counter==s2.length())
+            break;
+        if(s2[counter]==s1[i])
+        {
+            counter++;
+        }
+      else
+        {
+            // Special case where character preceding the i'th character is duplicate
+            if(counter > 0)
+            {
+                i -= counter;
+            }
+            counter = 0;
+        }
+    }
+    return counter < s2.length()?false:true;
+}
+
+
+void solve(){
+    static int c=1;
+    string s1, s2;
+    cin>>s1>>s2;
+    bool ans = Substr(s2, s1);
+    if(ans){
+        cout<<"Case #"<<c<<": "<<s1.length()-s2.length()<<"\n";
+    }
+    else{
+        cout<<"Case #"<<c<<": "<<"IMPOSSIBLE"<<"\n";
+    }
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+    while (t--) solve();
+    return 0;
+}
---
+++ daily contest/cf 753/1.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+#define int            long long int
+#define REP(i, a, b)   for(int i=a; i<=b; i++)
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define all(p)         p.begin(), p.end()
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define print(a)       for(auto x : a) cout << x << ' '; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << ' '  << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< ' '; cout << endl
+
+using namespace std;
+
+const int N = 200005;
+
+void solve(){
+    string s, f; 
+    cin>>s>>f;
+    int a[26]={0}, ans=0;
+    for(int i=0; i<s.length(); i++){
+        a[s[i]-'a'] = i; 
+    }
+    for(int i=0; i<f.length()-1; i++){
+        ans += abs(a[i]-a[i+1]);
+    }
+    cout<<ans<<"\n";
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+    while (t--) solve();
+    return 0;
+}
