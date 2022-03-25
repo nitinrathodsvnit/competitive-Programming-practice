30b625ca8267e7ce1e3d6d3ac68004d08c7d57b0
nitin rathod <nitinrathod2300@gmail.com>
2022-03-25 15:33:44 +0530

    1 done

---
+++ daily contest/long mar2/1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n; 
+    cin>>n;
+    n+=7;
+    if(n>170){
+        cout<<"YES";
+    }
+    else{cout<<"NO";}
+    return 0;
+}
