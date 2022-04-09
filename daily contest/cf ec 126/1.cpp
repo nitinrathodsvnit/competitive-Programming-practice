c7006a42247db8e0c1bb06f8494bf5bf6e8d61f0
nitin rathod <nitinrathod2300@gmail.com>
2022-04-09 20:37:44 +0530

    today

---
+++ daily contest/cf ec 126/1.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+#define int            long long int
+#define REP(i, a, b)   for(int i=a; i<=b; i++)
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define all(p)         p.begin(), p.end()
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define print(a)       for(auto x : a) cout << x << ' '; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << ' '  << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< ' '; cout << endl
+
+using namespace std;
+
+const int N = 200005;
+
+void solve(){
+    int n;
+    cin>>n;
+    int a[n], b[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    for(int i=0; i<n; i++){
+        cin>>b[i];
+    }
+    int ans=0;
+    for(int i=0; i<n-1; i++){
+        ans += abs(min(a[i], b[i]) - min(a[i+1], b[i+1]));
+        ans += abs(max(a[i], b[i]) - max(a[i+1], b[i+1]));
+    }
+    cout<<ans<<"\n";
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+    while (t--) solve();
+    return 0;
+}
