684446106f9f8f094403833f573e22e8a71f608e
nitin rathod <nitinrathod2300@gmail.com>
2022-04-12 18:56:17 +0530

    done

---
+++ C2-ladders/b_max_sum_of_digit.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+#define int            long long int
+#define REP(i, a, b)   for(int i=a; i<=b; i++)
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define all(p)         p.begin(), p.end()
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define print(a)       for(auto x : a) cout << x << ' '; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << ' '  << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< ' '; cout << endl
+
+using namespace std;
+
+const int N = 200005;
+
+int sumofdigit(int n){
+    int ans=0;
+    while(n){
+        ans += (n%10);
+        ans/=10;
+    }
+    return ans;
+}
+
+void solve(){
+    int n;
+    cin>>n;
+    if(n&1){
+        cout<<sumofdigit(n/2)+sumofdigit((n+1)/2);
+    }
+    else{
+        cout<<sumofdigit(n/2 - 1) + sumofdigit(n/2 + 1);
+    }
+    cout<<endl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+    while (t--) solve();
+    return 0;
+}
