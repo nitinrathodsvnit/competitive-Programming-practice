3ac077997ae1d7dc75d6086b9ffb9975558a6237
nitin rathod <nitinrathod2300@gmail.com>
2022-04-28 09:31:59 +0530

    gfg today done

---
+++ daily practices questions/Problem of the Day (gfg program)/28_4_2022_shop_in_candy_store.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> candyStore(int candies[], int N, int K)
+    {
+        // Write Your Code here
+        sort(candies, candies + N);
+        int tm = N, i = 0;
+        vector<int> ans;
+        ans.reserve(2);
+        ans[0] = ans[1] = 0;
+        while (tm > 0)
+        {
+            tm -= (K + 1);
+            ans[0] += candies[i];
+            ans[1] += candies[N - i - 1];
+            i++;
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N, K;
+        cin >> N >> K;
+        int candies[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            cin >> candies[i];
+        }
+
+        Solution ob;
+        vector<int> cost = ob.candyStore(candies, N, K);
+        cout << cost[0] << " " << cost[1] << endl;
+    }
+    return 0;
+} // } Driver Code Ends
