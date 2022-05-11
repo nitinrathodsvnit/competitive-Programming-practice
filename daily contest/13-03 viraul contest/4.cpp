a30e360d341cf3085d56fd315b68d470a7b5cbb3
nitin rathod <nitinrathod2300@gmail.com>
2022-05-11 17:49:08 +0530

    done

--- daily contest/13-03 viraul contest/4.cpp
+++ daily contest/13-03 viraul contest/4.cpp
@@ -7,6 +7,39 @@ const int N = 200005;
 
 void solve()
 {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (s[i] == 'W')
+            continue;
+        else
+        {
+            char cur = s[i];
+            int pos = i;
+
+            while (i < n && s[i] == cur)
+                i++;
+
+            bool ok1 = 1, ok2 = 1;
+
+            if (!pos || s[pos - 1] == 'W')
+                ok1 = 0;
+            if (i == n || s[i] == 'W')
+                ok2 = 0;
+
+            if (!ok1 && !ok2)
+            {
+                cout << "NO\n";
+
+                return;
+            }
+        }
+    }
+
+    cout << "YES\n";
 }
 
 int32_t main()
