c3e92e88deb06a5ef1e5c86ec3d977597549c5bf
nitin rathod <nitinrathod2300@gmail.com>
2022-05-11 08:07:23 +0530

    first done

--- daily contest/1 cf round 787 div 3/3.cpp
+++ daily contest/1 cf round 787 div 3/3.cpp
@@ -8,6 +8,33 @@ const int N = 200005;
 
 void solve()
 {
+    string s;
+    cin >> s;
+    int n = s.length();
+    int zero = 0, one = 0, q = 0, ans = 0;
+    int i = 0;
+    while (n--)
+    {
+        if (s[i] == '?')
+        {
+            ans++;
+        }
+        else if (s[i] == '0')
+        {
+            ans++;
+            cout << ans << "\n";
+            return;
+        }
+        if (s[i] == '1')
+        {
+            one = 1;
+            q = 0;
+            zero = 0;
+            ans = 1;
+        }
+        i++;
+    }
+    cout << ans << "\n";
 }
 
 int32_t main()
