06694d990673f8a0e2d38c50ea6e44a842146f4b
nitin rathod <nitinrathod2300@gmail.com>
2022-05-13 20:02:12 +0530

    update files

---
+++ daily contest/starters 38 11-05-22/1.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+#define int            long long int
+using namespace std;
+
+const int N = 200005;
+
+void solve(){
+    int a[3];
+    cin>>a[1]>>a[0]>>a[2];
+    sort(a, a+3);
+    if(a[2]>(a[0] + a[1])){
+        cout<<"YES\n";
+    }
+    else{
+        cout<<"NO\n";
+    }
+}
+
+
+int32_t main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+    while (t--) solve();
+    return 0;
+}
