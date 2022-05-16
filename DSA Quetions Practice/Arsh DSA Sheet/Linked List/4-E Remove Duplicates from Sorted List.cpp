8009ffbc12c70b80069ce7ab565ec0584809232f
nitin rathod <nitinrathod2300@gmail.com>
2022-05-16 21:11:39 +0530

    day 1

--- DSA Quetions Practice/Arsh DSA Sheet/Linked List/4-E Remove Duplicates from Sorted List.cpp
+++ DSA Quetions Practice/Arsh DSA Sheet/Linked List/4-E Remove Duplicates from Sorted List.cpp
@@ -1,3 +1,16 @@
 // Remove Duplicates from Sorted List
 
-Remove Duplicates from Sorted List
+ListNode *deleteDuplicates(ListNode *head)
+{
+    ListNode *t = head;
+    while (t)
+    {
+        while (t->next and t->val == t->next->val)
+        {
+            t->next = t->next->next;
+        }
+        t = t->next;
+    }
+
+    return head;
+}
