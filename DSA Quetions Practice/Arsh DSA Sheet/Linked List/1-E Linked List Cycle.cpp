d831cdc3c9e5072aa667e549ab37ef93fb6fba93
nitin rathod <nitinrathod2300@gmail.com>
2022-05-16 20:49:39 +0530

    day 1

---
+++ DSA Quetions Practice/Arsh DSA Sheet/Linked List/1-E Linked List Cycle.cpp
@@ -0,0 +1,22 @@
+/*
+Return true if there is a cycle in the linked list. Otherwise, return false
+
+solution :-
+    fast pointer slow pointer
+*/
+
+bool hasCycle(ListNode *head)
+{
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast and fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            return true;
+        }
+    }
+    return false;
+}
