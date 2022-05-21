3e91ed478796b7c5f8b1be64ab5df8cec0ccc66d
nitin rathod <nitinrathod2300@gmail.com>
2022-05-22 05:10:52 +0530

    day 3

---
+++ DSA Quetions Practice/Arsh DSA Sheet/Linked List/9-E  Intersection of Two Linked Lists.cpp
@@ -0,0 +1,43 @@
+/*
+input: listA = [4,1,8,4,5], listB = [5,6,1,8,4,5]
+Output: Intersected at '8'
+
+sol: firstly determine the size of both LinkedList if anyone is bigger,  then iterate till they both are not the same and then iterate both at the same loop and check when the both linkedlist are same or not and then return
+a new thing I larne: we can also swap the linked list swap(temp1, temp2)
+*/
+class Solution
+{
+public:
+    ListNode *getIntersectionNode(ListNode *A, ListNode *B)
+    {
+        string s1 = "", s2 = "";
+        ListNode *temp1 = A;
+        ListNode *temp2 = B;
+        int n1 = 0, n2 = 0;
+        while (A)
+        {
+            A = A->next;
+            n1++;
+        }
+        while (B)
+        {
+            B = B->next;
+            n2++;
+        }
+        int i = abs(n1 - n2);
+        if (n1 < n2)
+        {
+            swap(temp1, temp2);
+        }
+        while (i--)
+        {
+            temp1 = temp1->next;
+        }
+        while (temp1 != temp2)
+        {
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        return temp1;
+    }
+};
