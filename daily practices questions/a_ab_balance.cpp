3b32a0b469423ebe2f24c723c60f8eb7907e171e
nitin rathod <nitinrathod2300@gmail.com>
2022-06-07 14:55:55 +0530

    update file

--- daily practices questions/a_ab_balance.cpp
+++ daily practices questions/a_ab_balance.cpp
@@ -1,41 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
-#define int            long long int
-#define REP(i, a, b)   for(int i=a; i<=b; i++)
-#define F              first
-#define S              second
-#define pb             push_back
-#define si             set <int>
-#define vi             vector <int>
-#define pii            pair <int, int>
-#define vpi            vector <pii>
-#define vpp            vector <pair<int, pii>>
-#define mii            map <int, int>
-#define mpi            map <pii, int>
-#define spi            set <pii>
-#define all(p)         p.begin(), p.end()
-#define que_max        priority_queue <int>
-#define que_min        priority_queue <int, vi, greater<int>>
-#define print(a)       for(auto x : a) cout << x << ' '; cout << endl
-#define print1(a)      for(auto x : a) cout << x.F << ' '  << x.S << endl
-#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< ' '; cout << endl
-
+#define int long long int
 using namespace std;
 
 const int N = 200005;
 
-void solve(){
-    string s;
-    cin>>s;
-    int n = s.length();
-    s[0] = s[n-1];
-    cout<<s<<endl;
+void solve()
+{
+    int n;
+    cin>>n;
+    n = 2*n;
+    vector<string> v(n);
+    for(int i=0; i<n; i++){
+        cin>>v[i];
+    }
+    string st;
+    cin>>st;
+    int frq[26]={0};
+    for(int i=0; i<n; i++){
+        frq[st[i]-'a']++;
+    }
+    for(int i=0; i<n; i++){
+        
+    }
 }
 
-int32_t main(){
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int t=1;
-    cin>>t;
-    while (t--) solve();
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
     return 0;
 }
